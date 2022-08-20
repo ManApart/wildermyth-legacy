b832dbf1e4c6623eb789e5b8147b8e7c0fbeb396
ManApart <r@austinkucera.com>
2022-08-20 07:48:52 -0400

    most basic skeleton

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.8.0")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Simple.kt
+++ src/main/kotlin/Simple.kt
@@ -1,11 +1,26 @@
+import kotlinx.browser.*
+import kotlinx.html.*
+import kotlinx.html.dom.*
+
 fun main() {
-    console.log("Hello, ${greet()}")
+    displayExample()
 }
 
-fun greet() = "world"
-
 fun displayExample() {
+    val example = JSON.parse<Character>("{\"name\": \"Cob\"}")
 
+    document.body!!.append.div {
+        h1 {
+            +example.name
+        }
+        img {
+            src = "./example/default.png"
+        }
+        img {
+            src = "./example/body.png"
+        }
+
+    }
 }
 
 /*
@@ -16,4 +31,18 @@ Upload Body
 Upload data
 Save character in local storage
 Export thin legacy data?
+ */
+
+/*
+ var hero;
+
+  if ( localStorage.getItem('heroImg')) {
+    hero = localStorage.getItem('heroImg');
+  }
+  else {
+    hero = '/9j/4AAQSkZJRgABAgAAZABkAAD/7    /.../    6p+3dIR//9k=';
+    localStorage.setItem('heroImg',hero);
+  }
+
+  document.getElementById("hero-graphic").src='data:image/png;base64,' + hero;
  */
