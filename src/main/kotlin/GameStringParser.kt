ab75db5a9aa75a266600e30fd4e92024a641dac2
ManApart <r@austinkucera.com>
2022-08-28 12:43:44 -0400

    add gamestring parser

---
+++ src/main/kotlin/GameStringParser.kt
@@ -0,0 +1,11 @@
+fun Character.interpolate(line: String): String {
+    return line
+}
+
+/*
+TODO
+find all interpolation spots
+find an appropriate rule
+use the rule to replace
+default to last word
+ */
